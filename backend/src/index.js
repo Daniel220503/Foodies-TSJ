5140a3fc4c63a5023d1e2cee922ec5ddadb18b8a
Antoninox216 <aghr206@gmail.com>
2026-03-10 08:27:54 -0600

    DB conexion

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const pool = require("./config/db");
 
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Foodies API running");
+});
+
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API funcionando" });
+});
+
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected:", result.rows);
+  }
 });
 
 app.listen(PORT, () => {
