57937cb54e727e6a10813c80852a4dcd100b4a20
Tamalon verde <za230110877@zapopan.tecmm.edu.mx>
2026-03-10 21:05:09 -0600

    Actualizacion de la base de datos

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "tsj_postgres",   
+  host: "postgres",
   user: "postgres",
   password: "123456",
   database: "tsj_foodies",
   port: 5432
 });
+
 
 module.exports = pool;
